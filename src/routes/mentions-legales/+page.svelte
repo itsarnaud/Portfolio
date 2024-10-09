654625678e5b9f62f3c2db81070b2888499216e4
Arnaud R. <arno.royer2004@gmail.com>
2024-10-09 19:41:22 +0200

    Add terms of use

---
+++ src/routes/mentions-legales/+page.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+  <title>Mentions Légales</title>
+</svelte:head>
+
+<main class="flex flex-col p-4">
+  <h1 class="text-3xl font-bold mb-4 text-light-purple">Mentions Légales</h1>
+  
+  <h2 class="text-xl font-semibold mt-4">Éditeur du site</h2>
+  <p>Nom : Arnaud Royer</p>
+  <p>Email : arnaud.royer77@gmail.com</p>
+
+  <h2 class="text-xl font-semibold mt-4">Hébergement</h2>
+  <p>Ce site est hébergé par Vercel.</p>
+  <p>Adresse : 440 N Barranca Avenue #4133, Covina, CA 91723, United States</p>
+  <p>Email : privacy@vercel.com</p>
+
+  <h2 class="text-xl font-semibold mt-4">Propriété intellectuelle</h2>
+  <p>Tout le contenu de ce site, y compris les textes, images, graphismes, logos, est protégé par les droits d'auteur. Toute reproduction, même partielle, est interdite sans autorisation préalable.</p>
+
+  <h2 class="text-xl font-semibold mt-4">Données personnelles</h2>
+  <p>Les informations recueillies sur ce site sont destinées à un usage personnel. Aucune donnée personnelle n'est collectée sans votre consentement. Vous avez le droit d'accéder, de modifier et de supprimer vos données personnelles.</p>
+
+  <h2 class="text-xl font-semibold mt-4">Liens externes</h2>
+  <p>Ce site peut contenir des liens vers d'autres sites. Nous ne sommes pas responsables du contenu de ces sites externes.</p>
+
+  <h2 class="text-xl font-semibold mt-4">Contact</h2>
+  <p>Pour toute question concernant ces mentions légales, veuillez nous contacter à l'adresse email mentionnée ci-dessus.</p>
+</main>
