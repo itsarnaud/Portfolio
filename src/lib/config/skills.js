4de56673e25fd967c65a99a0f5ba28f6b181af79
Arnaud R. <arno.royer2004@gmail.com>
2024-10-09 18:47:48 +0200

    add skills

--- src/lib/config/skills.js
+++ src/lib/config/skills.js
@@ -7,17 +7,21 @@ export const skills = [
       { name: "JavaScript", image: "/skills/JavaScript.svg" },
       { name: "Svelte", image: "/skills/Svelte.svg" },
       { name: "Angular", image: "/skills/Angular.svg" },
+      { name: "React", image: "/skills/React.svg" },
       { name: "Tailwind", image: "/skills/Tailwind.svg" },
       { name: "Bootstrap", image: "/skills/Bootstrap.svg" },
+      { name: "ThreeJS", image: "/skills/ThreeJS.svg" },
     ]
   },
   {
     title: "Backend",
     skills: [
       { name: "PHP", image: "/skills/PHP.svg" },
+      { name: "Symfony", image: "/skills/Symfony.svg" },
       { name: "MySQL", image: "/skills/MySQL.svg" },
       { name: "TypeScript", image: "/skills/TypeScript.svg" },
       { name: "Java", image: "/skills/Java.svg" },
+      { name: "Spring", image: "/skills/Spring.svg" },
       { name: "Python", image: "/skills/Python.svg" },
       { name: "Nodejs", image: "/skills/Nodejs.svg" },
     ]
@@ -29,6 +33,13 @@ export const skills = [
       { name: "Illustrator", image: "/skills/Illustrator.svg" },
       { name: "Photoshop", image: "/skills/Photoshop.svg" },
       { name: "InDesign", image: "/skills/inDesign.svg" },
+    ]
+  },
+  {
+    title: "Autre",
+    skills: [
+      { name: "Markdown", image: "/skills/Markdown.svg" },
+      { name: "Git", image: "/skills/Git.svg" },
     ]
   }
 ];
