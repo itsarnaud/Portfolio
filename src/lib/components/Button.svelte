a7394a75bf8cd28d2dd026554de079ec43e18f21
Arnaud R. <arno.royer2004@gmail.com>
2025-05-13 00:52:49 +0200

    Feat: better description and UI

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,18 +1,30 @@
 <script>
-
   export let href = null;
   export let classes = '';
   export let download = null;
+  export let variant = 'default';
+  export let icon = null;
 
+  const VARIANTS = {
+    default: 'text-light-purple py-2 px-4 bg-white rounded-full duration-200 hover:ring-1 hover:ring-light-purple w-max',
+    icon: 'flex items-center justify-center p-2 bg-white rounded-full duration-200 hover:ring-1 hover:ring-light-purple w-10 h-10'
+  }
 </script>
 
 {#if href}
-  <a {href} {download} class="text-light-purple py-2 px-4 bg-white rounded-full duration-200 hover:ring-1 hover:ring-light-purple w-max {classes}">
-    <slot />
+  <a {href} {download} target="_blank" class="{VARIANTS[variant]} {classes}">
+    {#if icon && variant === 'icon'}
+      <i class={icon}></i>
+    {:else}
+      <slot />
+    {/if}
   </a>
 {:else}
-  <button class="text-light-purple py-2 px-4 bg-white rounded-full duration-200 hover:ring-1 hover:ring-light-purple w-max {classes}">
-    <slot />
+  <button class="{VARIANTS[variant]} {classes}">
+    {#if icon && variant === 'icon'}
+      <i class={icon}></i>
+    {:else}
+      <slot />
+    {/if}
   </button>
 {/if}
-
